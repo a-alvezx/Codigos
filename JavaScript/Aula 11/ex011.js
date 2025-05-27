0bc82064ea6ae213f0d64327a343bc9487e7b11d
a-alvezx <andrealvesmendes2006@gmail.com>
2025-05-26 22:11:39 -0300

    Aula 11, 10 e 09 do curso de JS

---
+++ JavaScript/Aula 11/ex011.js
@@ -0,0 +1,16 @@
+
+/* var vel = 8
+console.log (`A velocidade do seu carro é de ${vel}Km/h`)
+console.log ("Dirija sempre com cinto de segurança!")
+
+if (vel > 60 ) {
+    console.log (`Velocidade muito Alta. Multado!`)
+} */
+
+var pais = 'Brasil'
+console.log (`Vivendo em ${pais}`)
+if ( pais == 'Brasil'){
+    console.log ("Vc é Brasileiro")
+} else {
+    console.log ('Vc é Estrangeiro')
+}
